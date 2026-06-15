ef4aa00234e52017e1efd1b9bbe1b8d9cdd63ee0
malikzyni19 <malikzyni19@gmail.com>
2026-06-16 03:48:06 +0500

    Add Firebase web configuration for Live Monitor

---
+++ static/js/firebase-live-monitor-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCuLqKNWqRupqJPHSO4KkJi9BvPEeoW08w",
+  authDomain: "zyni-screener.firebaseapp.com",
+  projectId: "zyni-screener",
+  storageBucket: "zyni-screener.firebasestorage.app",
+  messagingSenderId: "631318513566",
+  appId: "1:631318513566:web:3ed49c020bb71600d0388f",
+};
+
+export const liveSetupsCollection = "live_setups";
